0d9433785ea65dc60838a047265e39875055c9c5
Pablo Colapinto <wolftype@gmail.com>
2012-09-12 18:28:25 -0700

    a lot of draw routines be reconfigured

---
+++ Static/vsr_static/vsr_static/Camera.h
@@ -0,0 +1,162 @@
+/*
+ *  CAMERA.h
+ *  gelatin
+ *
+ *  Created by x on 5/17/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+ 
+ #ifndef CAMERA_H_INCLUDED
+ #define CAMERA_H_INCLUDED
+ 
+#include "vsr_gxlib.h"
+#include "vsr_gl.h"
+#include "Frame.h"
+
+// #include <OPENGL/gl.h>
+// #include <GLUT/glut.h>
+ 
+namespace vsr {
+
+    struct Slab{
+        Vec blu, bru, tru, tlu;
+        Vec& operator[] (int i ) { return (&blu)[i]; }
+    };
+    
+    struct Volume{
+        Vec bl, br, tr, tl, blb, brb, trb, tlb; 
+        Vec& operator[] (int i ) { return (&bl)[i]; }
+    };
+    
+struct Frustrum{
+    float width, height, depth;
+    float bwidth, bheight;
+    
+    Slab dir;
+    Volume box;
+    
+    void calc(){
+        dir.blu = (box.blb - box.bl).unit();
+        dir.tru = (box.trb - box.tr).unit();
+        dir.bru = (box.brb - box.br).unit();
+        dir.tlu = (box.tlb - box.tl).unit();
+        width = box.br[0] - box.bl[0];
+        height = box.tr[1] - box.br[1];
+        depth = box.blb[2] - box.bl[2];
+        bwidth = box.brb[0] - box.blb[0];
+        bheight =box.trb[1] - box.brb[1];
+        
+    }
+    
+    Pln left(){ return Ro::null(box.tl) ^ Ro::null(box.bl) ^ Ro::null(box.blb) ^ Inf(1); }
+    Pln right(){ return Ro::null(box.tr) ^ Ro::null(box.br) ^ Ro::null(box.brb) ^ Inf(1); }
+    Pln top(){ return Ro::null(box.tl) ^ Ro::null(box.tr) ^ Ro::null(box.trb) ^ Inf(1); }
+    Pln bottom(){ return Ro::null(box.bl) ^ Ro::null(box.br) ^ Ro::null(box.brb) ^ Inf(1); }
+};
+    
+class Camera : public Frame {	
+
+		bool bOrtho;
+		GLdouble mFocal, mNear, mFar, mWidth, mHeight; 
+		
+		/* Transformation Matrices */
+		GLdouble mModel[16];
+		GLdouble mProj[16];
+		GLint	 mView[4];
+		
+		Vec mLook;													///< Direction to Look In
+		Frame mModelView;
+
+        Frustrum mFrustrum;
+//		Vec mForward;//Unit Direction to Look At ( mZ * -1)		
+//		Rot mModView;
+//		Pnt mLook;
+//		Mat4<> mImageMvm;
+				
+	public:
+		Camera(); 
+
+//		void createGui();					///< Add a Widget
+//		Gui& gui() { return *mGui; }
+
+		/* Implicit Getters and Setters */
+		void width( float w )	{ mWidth = w;}						///< set width
+		void height( float h )	{ mHeight = h;}						///< set height
+
+		float width()	const {return mWidth;}							///< get width
+		float height()	const {return mHeight;}							///< get height
+        float depth()   const { return mFar - mNear; }
+		
+		void	focal(float f)	{ mFocal = f; }						///< set focal length
+		double& focal()			{ return mFocal; }					///< get focal length
+		double	focal() const	{ return mFocal; }
+
+		void  perspective(bool b) { bOrtho = b; }
+		bool& perspective()  {return bOrtho; }
+		bool  perspective() const {return bOrtho; }
+										
+		void lookAt( const State& v ) { mLook = v; }
+						
+		Vec  look() { mLook = Vec(mPos) + forward();  return mLook; }		
+
+		void mvm( const Rot& r ) { mModelView.rot(r); }				///< Set Model View Matrix Rotor
+		Rot& mvm() { return mModelView.rot(); }						///< Model View Matrix Rotor
+		Rot  mvm()  const { return mModelView.rot(); }				///< Get Model View Matrix Rotor
+		Rot  cat()  const { return mRot * mModelView.rot(); }		///< Concatenate MVM and Camera Rotor
+		Rot  rcat() const { return mModelView.rot() * mRot; }		///< Reverse Concatenate MVM and Camera Rotor (not used)
+		Frame& modelView() { return mModelView; }					///< Get Model View Matrix Frame
+		Frame  modelView() const { return mModelView; }				///< Set Model View Matrix Frame
+		
+		Rot rat() const { return -mRot * mModelView.rot(); }		///< Ratio Rotor taking camera to mvm 
+		Rot rrat() const { return -mModelView.rot() * mRot; }		///< Reverse Ratio taking mvm to camera 
+		
+		Rot ryz() { return Gen::ratio_biv( modelView().yz(), yz() ); }	///< Get Ratio of YZ plane between Camera and MVM
+		Rot rxz() { return Gen::ratio_biv( modelView().xz(), xz() ); }	///< Get Ratio of XZ plane between Camera and MVM
+		Rot rxy() { return Gen::ratio_biv( modelView().xy(), xy() ); }	///< Get Ratio of XY plane between Camera and MVM
+				
+		void reset() { mvm( Rot(1,0,0,0) ); rot( Rot(1,0,0,0) ); pos(0,0,5); orient(); } 
+
+//		Camera& orientCat() {
+//			mImageMvm = Op::mat( mModView ); return *this;
+//		}
+		
+		Biv xycat(){ return xy() + mModelView.xy(); }//Op::sp0(xy(),-mvm()); }
+		Biv xzcat(){ return xz() + mModelView.xz(); }//Op::sp0(xz(),-mvm()); }
+		Biv yzcat(){ return yz() + mModelView.yz(); }//Op::sp0(yz(),-mvm()); }
+					
+		virtual void push3D();
+		virtual void pop3D();
+		
+		//ortho
+		void push2D(int, int);
+		void pop2D();
+		
+		//get mv info
+		const GLdouble * model() const { return mModel; }
+		const GLdouble * proj() const { return mProj; }
+		const GLint * view() const { return mView; }
+    
+    Frustrum frustrum() const { return mFrustrum; }
+    
+    Frustrum frustrum(){
+           mFrustrum.box.bl = GL::unproject(0,0,0.0, *this);
+           mFrustrum.box.blb = GL::unproject(0,0,1.0, *this);
+           mFrustrum.box.tr = GL::unproject( width(), height(), 0.0, *this);    
+           mFrustrum.box.trb = GL::unproject( width(), height(), 1.0, *this );        
+           mFrustrum.box.br = GL::unproject(width(),0,0.0, *this);
+           mFrustrum.box.brb = GL::unproject(width(),0,1.0, *this);
+           mFrustrum.box.tl = GL::unproject( 0, height(), 0.0, *this );    
+           mFrustrum.box.tlb = GL::unproject( 0,height(), 1.0, *this );  
+                 
+            mFrustrum.calc();
+        
+            return mFrustrum;
+
+        }
+
+};
+
+} //vsr::
+
+#endif
---
+++ Static/vsr_static/vsr_static/vsr_interp.h
@@ -0,0 +1,192 @@
+/*
+ *  vsr_interp.h
+ *  CONGA_07
+ *
+ *  Created by x on 5/29/11.
+ *  Copyright 2011 x. All rights reserved.
+ *
+ */
+
+#ifndef VSR_INTERP_INCLUDED
+#define VSR_INTERP_INCLUDED
+
+
+#include "State.h"
+#include "Matrix.h"
+
+using namespace std;
+
+namespace vsr {
+
+    enum Interpolation {
+        LINEAR,
+        QUADRIC,
+        CUBIC,
+        BSPLINE,
+        NURB,
+    };
+    
+   // template <typename T>
+	class Interp {
+		
+		public:
+			
+			/// t from 0 to 1
+            template<typename T>
+			static T cubic(T _a, T _b, T _c, T _d, double t);
+			/// t from -1 to 1
+            template<typename T>
+			static T quad(T _a, T _b, T _c, double t);
+			/// t from 0 to 1
+            template<typename T>
+			static T linear(T _a, T _b, double t);
+			
+			/// arbitrary number of points to pass through
+            template<typename T>
+			static T quad(T * s, int num, double t, bool closed =0);
+            /// arbitrary number of points to pass through
+            template<typename T>
+			static T cubic(T * s, int num, double t);
+        
+			//eulerian surface interpolation assumes four points 
+            template<typename T>
+			static T surface(T *s, double u, double v);
+            //eulerian surface interpolation assumes four points 
+            template<typename T>
+            static T surface(T a, T b, T c, T d, double u, double v);
+			//eulerian volume interpolation assumes eight points
+			template<typename T>
+            static T volume(T *s, double x, double y, double z);
+
+//            template<typename T>
+//            static vector<double> svals(T a, T b, T c, T d, double u, double v);
+//
+//            template<typename T>
+//            static vector<double> svals(T a, T b, T c, T d, double u, double v);
+
+	};
+    
+    template<typename T> 
+    inline T Interp :: cubic(T _a, T _b, T _c, T _d, double t) {
+        
+        //get three states describing HULL	
+        T a = _b - _a;
+        T b = _c - _b;
+        T c = _d - _c;
+        
+        //multiply each by p
+        T a2 = _a + a * t;
+        T b2 = _b + b * t;
+        T c2 = _c + c * t;
+        
+        //get second order translation states
+        a = b2 - a2;
+        b = c2 - b2;
+        
+        a2 += a * t;
+        b2 += b * t;
+        
+        //get third order (cubic)
+        a = b2 - a2;
+        a2 += a * t;
+        
+        return a2;
+    }
+    
+    //return state
+    template<typename T>
+    inline  T Interp :: quad(T _a, T _b, T _c, double t) {
+        
+        
+        //remap from 0 - 1 to -1 to 1
+        double nt = -1.0 + 2.0*t;
+        //	cout << " nt: " << nt << "\n";
+        
+        //get three states describing HULL	
+        double p = nt * nt; 
+        
+        T a = ( ( (_a + _c) * .5 )  - _b ) * p;
+        T b = (_c - _a ) * (.5 * nt);
+        T c = a + b + _b;
+        
+        return c;
+    }
+    
+    template<typename T>    
+    inline T Interp  :: linear(T _a, T _b, double t) {
+        return _a * (1-t) + _b * (t);
+    }
+    
+    template<typename T>
+    inline T Interp :: quad( T * cp, int num, double t, bool closed ) {
+        
+        if (t < 0 ) t = 0;
+        
+        
+        double n = closed ? num / 2.0 : (num-1)/2.0; 
+        
+        int fn = floor(n);		// number of 3 group sections
+        double rem = n - fn;	//remainder ( 0 or .5 )
+        
+        double td = ( t * n ); //fn?	
+        int it = floor(td); //current group
+        
+        double ct = td - it;   //current position in group (0-1)
+        
+        int gt = it * 2;	
+        
+        //	cout << "num: " << fn << " rem: " << rem << " group: " << it << " idx: " << gt << " ct: " << ct << "\n"; 
+        
+        if ( (rem != 0) && (it==fn) ) {
+            return closed? linear( cp[gt], cp[0], ct *2 ) : linear( cp[gt], cp[gt+1], ct *2 );
+        }
+        else{ 
+            return closed ? quad(cp[gt], cp[gt+1], (it==fn-1)? cp[0] : cp[gt+2], ct) : quad(cp[gt], cp[gt+1], cp[gt+2], ct); 	
+        }
+        
+    }
+    
+    //d c
+    //a b
+    template<typename T>
+    inline T Interp :: surface(T * cp, double u, double v){
+        T bot = cp[0] * (1-u) + cp[1] * u;
+        T top = cp[2] * (1-u) + cp[3] * u;
+        return bot * (1-v) + top * v;
+    }
+
+    template<typename T>
+    inline T Interp :: surface(T a, T b, T c, T d, double u, double v){
+        T bot = a * (1-u) + b * u;
+        T top = d * (1-u) + c * u;
+        return bot * (1-v) + top * v;
+    }
+    
+    
+    //d3 c2					h7 g6
+    //a0 b1  <- front		e4 f5  <- back
+    
+    //2 6					3 7
+    //0 4					1 5
+   template<typename T>
+     inline T Interp :: volume(T * cp, double u, double v, double w){
+        T fbottom = cp[0] * (1-u) + cp[4] * u;
+        T ftop = cp[2] * (1-u) + cp[6] * u;
+        T fmid = fbottom * (1-v) + ftop * (v);
+        
+        T bbottom = cp[1] * (1-u) + cp[5] * u;
+        T btop = cp[3] * (1-u) + cp[7] * u;
+        T bmid = bbottom * (1-v) + btop * (v);	
+        
+        return fmid * (1-w) + bmid * w;
+    }
+    
+//    template<typename T>
+//    inline T Interp :: svals(T a, T b, T c, T d, double u, double v){
+//        T bot = a * (1-u) + b * u;
+//        T top = d * (1-u) + c * u;
+//        return bot * (1-v) + top * v;
+//    }
+        
+}
+#endif
