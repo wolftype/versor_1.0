77e3adfc23b2a3ffbff9b1af4886519720e39ff0
ubob <wolftype@gmail.com>
2012-10-09 13:24:21 -0700

    remove local glv, use github to download instead

--- externals/glv/example/example.h
+++
@@ -1,9 +0,0 @@
-/*	Graphics Library of Views (GLV) - GUI Building Toolkit
-	See COPYRIGHT file for authors and license information */
-
-#include <math.h>
-#include "glv.h"
-#include "glv_binding.h"
-#include "glv_util.h"
-
-using namespace glv;
