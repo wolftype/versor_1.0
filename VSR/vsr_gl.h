e768f8b7680474ea48a6a17c7d1c756376324236
Pablo Colapinto <wolftype@gmail.com>
2012-11-21 06:40:55 -0800

    a bit of tutorialness

--- VSR/vsr_gl.h
+++ VSR/vsr_gl.h
@@ -224,7 +224,7 @@ namespace vsr {
       //  static bool isLightOn() { return bLights; } 
         
 		
-//        static GLenum Lights[8] = { GL_LIGHT0, GL_LIGHT1, GL_LIGHT2, GL_LIGHT3, GL_LIGHT4, GL_LIGHT5, GL_LIGHT6, GL_LIGHT7 };
+        static GLenum Lights[8] = { GL_LIGHT0, GL_LIGHT1, GL_LIGHT2, GL_LIGHT3, GL_LIGHT4, GL_LIGHT5, GL_LIGHT6, GL_LIGHT7 };
 		
         
         /// Project TO Screen Coordinates
