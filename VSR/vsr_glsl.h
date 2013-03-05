cf7b3f3de34760b1f8a65ea5081c7eefb0fef556
Pablo Colapinto <wolftype@gmail.com>
2013-03-05 08:50:00 -0800

    new readme and glsl

---
+++ VSR/vsr_glsl.h
@@ -0,0 +1,14 @@
+//
+//  vsr_glsl.h
+//  Versor
+//
+//  Created by Pablo Colapinto on 2/28/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef Versor_vsr_glsl_h
+#define Versor_vsr_glsl_h
+
+
+
+#endif
