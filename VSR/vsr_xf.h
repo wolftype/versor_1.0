41ba81d691fefca70b2f44ece87cc4676ab01a74
Pablo Colapinto <wolftype@gmail.com>
2013-04-25 21:25:57 -0700

    vsr_xf and vsr_matrix fixes for es 2.0

--- VSR/vsr_xf.h
+++ VSR/vsr_xf.h
@@ -60,7 +60,36 @@ namespace vsr {
                           zi[0] * s, zi[1] * s, zi[2] * s, 0,
                           x  ,   y ,   z ,    1 );
         }
-        
+ 
+        /*!
+         4x4 Transformation Matrix From Translation Vector, and Scale
+        */
+        inline Mat4f mat( const Vec& v, double s) {
+                    
+            double x = v[0]; double y = v[1]; double z = v[2];
+  
+            return Mat4f( s , 0 , 0 , 0, 
+                          0 , s , 0 , 0,
+                          0 , 0 , s , 0,
+                          x  ,   y ,   z ,    1 );
+        }
+  
+        /*!
+         4x4 Transformation Matrix From Rotor, Translation Vector, and Scale
+        */
+        inline Mat4f mat( const Rot& r, const Vec& v) {
+          
+            Vec xi = Vec::x.sp(r);
+            Vec yi = Vec::y.sp(r);
+            Vec zi = Vec::z.sp(r);
+            
+            double x = v[0]; double y = v[1]; double z = v[2];
+            
+            return Mat4f( xi[0] , xi[1] , xi[2] , 0, 
+                          yi[0] , yi[1] , yi[2] , 0,
+                          zi[0] , zi[1] , zi[2] , 0,
+                          x  ,   y ,   z ,    1 );
+        }     
         /*
         4x4 Transformation matrix from Circle
         */
@@ -70,6 +99,33 @@ namespace vsr {
             Vec v = Ro::loc(s);
             double scale = Ro::rad( s );
             return mat(r,v,scale);
+        }
+
+        /*
+        4x4 Transformation matrix from Vector
+        */
+        inline Mat4f mat(const Vec& v){
+            Rot r = Gen::ratio( Vec::z, v.unit() );	
+			return mat( r, v );
+        }
+
+        /*
+        4x4 Transformation matrix from dual sphere with known size
+        */
+        inline Mat4f mat(const Dls& v, double s){
+			
+			Pnt p = Ro::cen( v );
+			return mat( Vec(p), s );
+        }
+
+        /*
+        4x4 Transformation matrix from dual sphere 
+        */
+        inline Mat4f mat(const Dls& v ){
+		
+			double ta = Ro::size( v, true );		
+		    Pnt p = Ro::cen( v );
+			return mat( Vec(p), sqrt( fabs(ta) ) );
         }
 
     } // XF
