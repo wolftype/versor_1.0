701396560424ab5b1033b9bb6e406df4c12e4be8
Pablo Colapinto <wolftype@gmail.com>
2013-04-30 14:04:43 -0700

    vsr coord and mesh

--- VSR/vsr_coord.h
+++ VSR/vsr_coord.h
@@ -12,6 +12,12 @@
 #include "vsr.h"
 #include "vsr_op.h"
 
+#include <iostream>
+//#include <sstream>
+
+
+using namespace std;
+
 namespace vsr {
   
   namespace Coord{
@@ -19,10 +25,15 @@ namespace vsr {
     struct Sph{
         Sph( double t, double p, double r) : theta(t), phi(p), rad(r) {}
         double theta, phi, rad;
+        friend ostream& operator << (ostream& os, const Sph& s);
     };
     
+    inline ostream& operator << (ostream& os, const Sph& s){
+        os << "az: " << s.theta << "\telev: " << s.phi << "\tdist: " << s.rad;
+        return os;
+    }
+    
     inline Vec sph2vec(double theta, double phi){
-     
         Rot r = Gen::rot(theta/2.0, phi/2.0);
         return Vec::x.sp(r);   
     }
@@ -31,6 +42,20 @@ namespace vsr {
         double r = tv.norm();
         return Sph( atan2(tv[2],tv[0]) , acos(-tv[1]/r) - PIOVERTWO, r);
      } 
+     
+     //to degree format with 0 straight ahead
+     inline Sph sph2deg( const Sph& cs ){
+        double a = ( 180. * ( ( cs.theta / PI ) + 1. )  ) - 90.;
+        if (a < 0 ) a = 360 + a;
+        double e = 90. * cs.phi / PIOVERTWO;
+        double r = cs.rad;
+        return Sph( a, e, r );
+     }
+     
+     template<typename T>
+     inline Sph vec2deg( const T& v ){
+        return sph2deg( vec2sph(v) );
+     }
   }
 } //vsr::
 
