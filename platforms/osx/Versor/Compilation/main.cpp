ba91451783379fac21d74dba03ff67e83e570572
Pablo Colapinto <wolftype@gmail.com>
2013-07-23 08:11:31 -0700

    gfx changes

---
+++ platforms/osx/Versor/Compilation/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Compilation
+//
+//  Created by Pablo Colapinto on 7/11/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
